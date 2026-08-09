a06077ec9b9c3fd848dc812033a65f631a4ea357
anurag123rat <ay855128@gmail.com>
2026-08-09 21:00:35 +0530

    fixed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -49,11 +49,13 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/api/auth",
     });
+
+    //process.env.NODE_ENV === "production"
 
     res.status(200).json({
       accessToken,
