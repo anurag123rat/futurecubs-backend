8b97fe1ee924612bd5915e1423557c6f61061e93
anurag123rat <ay855128@gmail.com>
2026-08-09 21:08:33 +0530

    fixed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -52,7 +52,7 @@ export const login = async (req: Request, res: Response) => {
       secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: "/api/auth",
+      // path: "/api/auth",
     });
 
     //process.env.NODE_ENV === "production"
